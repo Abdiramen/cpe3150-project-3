ccb9c00ee52d42be90cdc0aabaad6c351db85c40
Illya Starikov <IllyaStarikov@iCloud.com>
2016-11-10 14:51:20 -0600

    Added files for game
    Added files (project files for Xcode) and main.c`

---
+++ code/space-invaders/space-invaders/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  space-invaders
+//
+//  Created by Illya Starikov on 11/10/16.
+//  Copyright © 2016 Illya Starikov. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
